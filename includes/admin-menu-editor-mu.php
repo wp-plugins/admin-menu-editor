ad67001c24a538af4767d85da93f77519620d19b
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-19 19:11:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/admin-menu-editor/trunk@254821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin-menu-editor-mu.php
+++ includes/admin-menu-editor-mu.php
@@ -18,9 +18,12 @@ mu-plugins/
 **/
 
 //Load the plugin
-$ws_menu_editor_filename = dirname(__FILE__) . '/admin-menu-editor/menu-editor.php'; 
+$ws_menu_editor_filename = dirname(__FILE__) . '/admin-menu-editor/menu-editor.php';
+$ws_menu_editor_pro_filename = dirname(__FILE__) . '/admin-menu-editor-pro/menu-editor.php';
 if ( file_exists($ws_menu_editor_filename) ) {
 	require $ws_menu_editor_filename;
+} elseif ( file_exists($ws_menu_editor_pro_filename) ) {
+	require $ws_menu_editor_pro_filename;
 } else {
 	add_action('admin_notices', 'ws_ame_installation_error');
 }
@@ -34,7 +37,7 @@ function ws_ame_installation_error(){
 		<p>
 		Please copy the entire <code>admin-menu-directory</code> directory to your <code>mu-plugins</code> 
 		directory, then move only the admin-menu-editor-mu.php file from
-		<code>admin-menu-editor</code> to <code>mu-plugins</code>.
+		<code>admin-menu-editor/includes</code> to <code>mu-plugins</code>.
 		</p> 
 </div>
 <?php
