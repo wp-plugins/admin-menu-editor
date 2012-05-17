ef9e102d51d83de3e300bef66df4557b25d80424
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-17 12:58:04 +0000

    Moved some menu manipulation functions to includes/menu.php
    git-svn-id: https://plugins.svn.wordpress.org/admin-menu-editor/trunk@545356 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-editor-core.php
+++ includes/menu-editor-core.php
@@ -498,61 +498,8 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 		}
 
 		//Resort the tree to ensure the found items are in the right spots
-		$tree = $this->sort_menu_tree($tree);
+		$tree = ameMenu::sort_menu_tree($tree);
 
-		return $tree;
-	}
-
-  /**
-   * Convert the WP menu structure to the internal representation. All properties set as defaults.
-   *
-   * @param array $menu
-   * @param array $submenu
-   * @return array Menu in the internal tree format.
-   */
-	function wp2tree($menu, $submenu){
-		$tree = array();
-		foreach ($menu as $pos => $item){
-			
-			$tree_item = ameMenuItem::blank_menu();
-			$tree_item['defaults'] = ameMenuItem::fromWpItem($item, $pos);
-			$tree_item['separator'] = $tree_item['defaults']['separator'];
-			
-			//Attach sub-menu items
-			$parent = $tree_item['defaults']['file'];
-			if ( isset($submenu[$parent]) ){
-				foreach($submenu[$parent] as $position => $subitem){
-					$tree_item['items'][$subitem[2]] = array_merge(
-						ameMenuItem::blank_menu(),
-						array('defaults' => ameMenuItem::fromWpItem($subitem, $position, $parent))
-					);
-				}				
-			}
-			
-			$tree[$parent] = $tree_item;
-		}
-
-		$tree = $this->sort_menu_tree($tree);
-
-		return $tree;
-	}
-
-  /**
-   * Sort the menus and menu items of a given menu according to their positions 
-   *
-   * @param array $tree A menu structure in the internal format
-   * @return array Sorted menu in the internal format
-   */
-	function sort_menu_tree($tree){
-		//Resort the tree to ensure the found items are in the right spots
-		uasort($tree, 'ameMenuItem::compare_position');
-		//Resort all submenus as well
-		foreach ($tree as &$topmenu){
-			if (!empty($topmenu['items'])){
-				uasort($topmenu['items'], 'ameMenuItem::compare_position');
-			}
-		}
-		
 		return $tree;
 	}
 
@@ -759,7 +706,7 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 		);
 
 		//Build a tree struct. for the default menu
-		$default_tree = $this->wp2tree($this->default_wp_menu, $this->default_wp_submenu);
+		$default_tree = ameMenu::wp2tree($this->default_wp_menu, $this->default_wp_submenu);
 		$default_menu = ameMenu::load_array($default_tree);
 
 		//Is there a custom menu?
--- includes/menu.php
+++ includes/menu.php
@@ -96,6 +96,59 @@ abstract class ameMenu {
 		$menu = self::add_format_header($menu);
 		return json_encode($menu);
 	}
+
+  /**
+   * Sort the menus and menu items of a given menu according to their positions
+   *
+   * @param array $tree A menu structure in the internal format (just the tree).
+   * @return array Sorted menu in the internal format
+   */
+	public static function sort_menu_tree($tree){
+		//Resort the tree to ensure the found items are in the right spots
+		uasort($tree, 'ameMenuItem::compare_position');
+		//Resort all submenus as well
+		foreach ($tree as &$topmenu){
+			if (!empty($topmenu['items'])){
+				uasort($topmenu['items'], 'ameMenuItem::compare_position');
+			}
+		}
+
+		return $tree;
+	}
+
+   /**
+	* Convert the WP menu structure to the internal representation. All properties set as defaults.
+	*
+	* @param array $menu
+	* @param array $submenu
+	* @return array Menu in the internal tree format.
+	*/
+	public static function wp2tree($menu, $submenu){
+		$tree = array();
+		foreach ($menu as $pos => $item){
+
+			$tree_item = ameMenuItem::blank_menu();
+			$tree_item['defaults'] = ameMenuItem::fromWpItem($item, $pos);
+			$tree_item['separator'] = $tree_item['defaults']['separator'];
+
+			//Attach sub-menu items
+			$parent = $tree_item['defaults']['file'];
+			if ( isset($submenu[$parent]) ){
+				foreach($submenu[$parent] as $position => $subitem){
+					$tree_item['items'][$subitem[2]] = array_merge(
+						ameMenuItem::blank_menu(),
+						array('defaults' => ameMenuItem::fromWpItem($subitem, $position, $parent))
+					);
+				}
+			}
+
+			$tree[$parent] = $tree_item;
+		}
+
+		$tree = self::sort_menu_tree($tree);
+
+		return $tree;
+	}
 }
 
 
