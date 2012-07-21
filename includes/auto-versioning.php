40c956626eb3294658bbdf666c46ba625ef5e9c5
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-21 09:42:14 +0000

    Renamed a lot of stuff. I'm still not fully satisfied with the class name and some of the functions.
    git-svn-id: https://plugins.svn.wordpress.org/admin-menu-editor/trunk@575424 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/auto-versioning.php
+++ includes/auto-versioning.php
@@ -1,28 +1,24 @@
 <?php
 
-if ( !class_exists('AutoVersioningSupport') ) {
+if ( !class_exists('DependencyAutoVersioner') ) {
 
 /**
  * @link http://stackoverflow.com/questions/118884/what-is-an-elegant-way-to-force-browsers-to-reload-cached-css-js-files
  */
-class AutoVersioningSupport {
+class DependencyAutoVersioner {
 	private static $version_in_filename = true;
 
-	public static function add_dependency($callback, $handle, $src, $deps, $last_param, $add_ver_to_filename = true ) {
-		$version = false;
-		if ( !empty($src) ) {
-			list($src, $version) = self::auto_version($src, $add_ver_to_filename);
-		}
-		call_user_func($callback, $handle, $src, $deps, $version, $last_param);
+	public static function add_dependency($wp_api_function, $handle, $src, $deps, $last_param, $add_ver_to_filename = true ) {
+		list($src, $version) = self::auto_version($src, $add_ver_to_filename);
+		call_user_func($wp_api_function, $handle, $src, $deps, $version, $last_param);
 	}
 
 	private static function auto_version($url, $add_ver_to_filename = true) {
-		/** @var WP_Rewrite $wp_rewrite */
-		global $wp_rewrite;
-
-		$path = self::guess_filename_from_url($url);
+		global $wp_rewrite; /** @var WP_Rewrite $wp_rewrite */
 
 		$version = false;
+		$path = self::guess_filename_from_url($url);
+
 		if ( ($path !== null) && is_file($path) ) {
 			$mtime = filemtime($path);
 			if ( $add_ver_to_filename && $wp_rewrite->using_mod_rewrite_permalinks() ) {
@@ -59,15 +55,15 @@ class AutoVersioningSupport {
 		return $path;
 	}
 
-	public static function auto_version_everything($add_ver_to_filename = true) {
+	public static function apply_to_all_dependencies($add_ver_to_filename = true) {
 		self::$version_in_filename = $add_ver_to_filename;
 		foreach(array('script_loader_src', 'style_loader_src') as $hook) {
-			add_filter($hook, __CLASS__ . '::dependency_loader_src', 10, 1);
+			add_filter($hook, __CLASS__ . '::_filter_dependency_src', 10, 1);
 		}
 	}
 
-	public static function dependency_loader_src($src) {
-		//Only add version info to CSS/JS files that don't already include it in the file name.
+	public static function _filter_dependency_src($src) {
+		//Only add version info to CSS/JS files that don't already have it in the file name.
 		if ( preg_match('@(?<!\.\d{10})\.(css|js)(\?|$)@i', $src) ) {
 			list($src, $version) = self::auto_version($src, self::$version_in_filename);
 			if ( !empty($version) ) {
@@ -80,41 +76,26 @@ class AutoVersioningSupport {
 
 } //class_exists()
 
-//AutoVersioningSupport::auto_version_everything(false);
-
-
 if ( !function_exists('wp_register_auto_versioned_script') ) {
-	/**
-	 * Like wp_register_script(), except it auto-versions the URL on file modification time.
-	 */
 	function wp_register_auto_versioned_script($handle, $src, $deps = array(), $in_footer = false, $add_ver_to_filename = true) {
-		AutoVersioningSupport::add_dependency('wp_register_script', $handle, $src, $deps, $in_footer, $add_ver_to_filename);
+		DependencyAutoVersioner::add_dependency('wp_register_script', $handle, $src, $deps, $in_footer, $add_ver_to_filename);
 	}
 }
 
 if ( !function_exists('wp_register_auto_versioned_style') ) {
-	/**
-	 * Like wp_register_style(), except it auto-versions the style sheet URL on file modification time.
-	 */
 	function wp_register_auto_versioned_style( $handle, $src, $deps = array(), $media = 'all', $add_ver_to_filename = true ) {
-		AutoVersioningSupport::add_dependency('wp_register_style', $handle, $src, $deps, $media, $add_ver_to_filename);
+		DependencyAutoVersioner::add_dependency('wp_register_style', $handle, $src, $deps, $media, $add_ver_to_filename);
 	}
 }
 
 if ( !function_exists('wp_enqueue_auto_versioned_script') ) {
-	/**
-	 * Like wp_enqueue_script(), except the script is auto-versioned based on file modification time.
-	 */
-	function wp_enqueue_auto_versioned_script( $handle, $src = false, $deps = array(), $in_footer = false, $add_ver_to_filename = true ) {
-		AutoVersioningSupport::add_dependency('wp_enqueue_script', $handle, $src, $deps, $in_footer, $add_ver_to_filename);
+	function wp_enqueue_auto_versioned_script( $handle, $src, $deps = array(), $in_footer = false, $add_ver_to_filename = true ) {
+		DependencyAutoVersioner::add_dependency('wp_enqueue_script', $handle, $src, $deps, $in_footer, $add_ver_to_filename);
 	}
 }
 
 if ( !function_exists('wp_enqueue_auto_versioned_style') ) {
-	/**
-	 * Like wp_enqueue_style(), except the style is auto-versioned based on file modification time.
-	 */
-	function wp_enqueue_auto_versioned_style( $handle, $src = false, $deps = array(), $media = 'all', $add_ver_to_filename = true ) {
-		AutoVersioningSupport::add_dependency('wp_enqueue_style', $handle, $src, $deps, $media, $add_ver_to_filename);
+	function wp_enqueue_auto_versioned_style( $handle, $src, $deps = array(), $media = 'all', $add_ver_to_filename = true ) {
+		DependencyAutoVersioner::add_dependency('wp_enqueue_style', $handle, $src, $deps, $media, $add_ver_to_filename);
 	}
 }
