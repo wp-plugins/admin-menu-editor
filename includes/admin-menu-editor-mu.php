110e2575e87ee2299ff11fa6ac8d46da1e5ef721
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-29 12:07:25 +0000

    Update the plugin name of the multisite module so that it's possible to differentiate between that and the main file in the WP UI.
    git-svn-id: https://plugins.svn.wordpress.org/admin-menu-editor/trunk@578681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin-menu-editor-mu.php
+++ includes/admin-menu-editor-mu.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Admin Menu Editor Pro
+Plugin Name: Admin Menu Editor Pro (Multisite module)
 Plugin URI: http://w-shadow.com/admin-menu-editor-pro/
 Description: Lets you edit the WordPress admin menu. To access the editor, go to the Dashboard of one of your network sites and open the Settings -&gt; Menu Editor page.
 Author: Janis Elsts
