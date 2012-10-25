c2de237acde5d2862832fe706dcd620b47407909
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-25 14:34:59 +0000

    Track what capabilities are assigned to what pages (URLs) and try to intelligently decide which one to pick if there are multiple options.
     The problem: Different menu items can point to the same URL. Each menu item has its own set of access permissions. Given a URL, how do we determine if the current user can access it?
       
    Previously, the plugin would just pick the last submenu item that matches the URL and use that  (if no submenus matched the URL, it would use the first top level menu that does). This was implemented through the reverse_item_lookup array. The downside was that:
     
     a) It was inconsistent - different menus that point to the same URL could still have different capabilities in the final menu structure, even if only one of them was actually used to check page access.
     b) When the plugin couldn't correctly identify a menu (= "missing" item), it would use the default permissions even if there was an existing item in the custom tree with the same URL and custom permissions. Specifically, this was a problem with the Arctica theme. This theme has multiple admin pages, some of which are only accessible to administrators, some for other roles. If it discovers that the current user can't access its primary top level menu, it will turn one of its  submenus into a top level menu. This breaks menu identification in AME.
      
      This change works around the problem by explicitly tracking capabilities assigned to each page URL and picking the one with the highest priority (custom permissions > default ones, even if the template is missing).
      
      If we could rely on slug (aka "file") being unique for each menu, we wouldn't need to go to such ridiculous lengths to identify menus.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/admin-menu-editor/trunk@617348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-editor-core.php
+++ includes/menu-editor-core.php
@@ -37,6 +37,12 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
                                             //indexed by URL.
 
 	/**
+	 * @var array List of per-URL capabilities, indexed by priority. Used while merging and
+	 * building the final admin menu.
+	 */
+	private $page_access_lookup = array();
+
+	/**
 	 * @var array The current custom menu with defaults merged in.
 	 */
 	private $merged_custom_menu = null;
@@ -646,6 +652,10 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 					//Record the menu as missing, unless it's a menu separator
 					if ( empty($topmenu['separator']) ){
 						$topmenu['missing'] = true;
+
+						$temp = ameMenuItem::apply_defaults($topmenu);
+						$temp['access_level'] = $this->get_menu_capability($temp);
+						$this->add_access_lookup($temp, 'menu', true);
                     }
 				}
 			}
@@ -664,6 +674,10 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 						} else {
 							//Record as missing
 							$item['missing'] = true;
+
+							$temp = ameMenuItem::apply_defaults($item);
+							$temp['access_level'] = $this->get_menu_capability($temp);
+							$this->add_access_lookup($temp, 'submenu', true);
                         }
 					}
 				}
@@ -729,6 +743,49 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 		return $tree;
 	}
 
+	/**
+	 * Add a page and its required capability to the page access lookup.
+	 *
+	 * The lookup array is indexed by priority. Priorities (highest to lowest):
+	 *      - Has custom permissions and a known template.
+	 *      - Has custom permissions, template missing or can't be determined correctly.
+	 *      - Default permissions.
+	 *      - Everything else.
+	 * Additionally, submenu items have slightly higher priority that top level menus.
+	 * The desired end result is for menu items with custom permissions to override
+	 * default menus.
+	 *
+	 * Note to self: If we were to keep items with an unknown template instead of throwing
+	 * them away during the merge phase, we could simplify this considerably.
+	 *
+	 * @param array $item Menu item (with defaults already applied).
+	 * @param string $item_type 'menu' or 'submenu'.
+	 * @param bool $missing Whether the item template is missing or unknown.
+	 */
+	private function add_access_lookup($item, $item_type = 'menu', $missing = false) {
+		if ( empty($item['url']) ) {
+			return;
+		}
+
+		$has_custom_settings = !empty($item['grant_access']) || !empty($item['extra_capability']);
+		$priority = 6;
+		if ( $missing ) {
+			if ( $has_custom_settings ) {
+				$priority = 4;
+			} else {
+				return; //Don't even consider missing menus without custom access settings.
+			}
+		} else if ( $has_custom_settings ) {
+			$priority = 2;
+		}
+
+		if ( $item_type == 'submenu' ) {
+			$priority--;
+		}
+
+		$this->page_access_lookup[$item['url']][$priority] = $item['access_level'];
+	}
+
   /**
    * Generate WP-compatible $menu and $submenu arrays from a custom menu tree.
    * 
@@ -746,6 +803,7 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
    * @return void
    */
 	function build_custom_wp_menu($tree){
+		$new_tree = array();
 		$new_menu = array();
 		$new_submenu = array();
 		$this->title_lookups = array();
@@ -771,16 +829,13 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 			$first_nonseparator_found = true;
 
 			$topmenu = $this->prepare_for_output($topmenu, 'menu');
-			$new_menu[] = $this->convert_to_wp_format($topmenu);
 
 			if ( empty($topmenu['separator']) ) {
 				$this->title_lookups[$topmenu['file']] = !empty($topmenu['page_title']) ? $topmenu['page_title'] : $topmenu['menu_title'];
-				if ( !array_key_exists($topmenu['url'], $this->reverse_item_lookup) ) { //Prefer sub-menus.
-					$this->reverse_item_lookup[$topmenu['url']] = $topmenu;
-				}
 			}
 				
 			//Prepare the submenu of this menu
+			$new_items = array();
 			if( !empty($topmenu['items']) ){
 				$items = $topmenu['items'];
 				//Sort by position
@@ -793,12 +848,41 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 					}
 
 					$item = $this->prepare_for_output($item, 'submenu', $topmenu['file']);
-					$new_submenu[$topmenu['file']][] = $this->convert_to_wp_format($item);
+					$new_items[] = $item;
 
 					//Make a note of the page's correct title so we can fix it later if necessary.
 					$this->title_lookups[$item['file']] = !empty($item['page_title']) ? $item['page_title'] : $item['menu_title'];
-					$this->reverse_item_lookup[$item['url']] = $item;
 				}
+			}
+
+			$topmenu['items'] = $new_items;
+			$new_tree[] = $topmenu;
+		}
+
+		//Use only the highest-priority capability for each URL.
+		foreach($this->page_access_lookup as $url => $capabilities) {
+			ksort($capabilities);
+			$this->page_access_lookup[$url] = reset($capabilities);
+		}
+
+		//var_dump($this->page_access_lookup);
+		//Convert the prepared tree to the internal WordPress format.
+		foreach($new_tree as $topmenu) {
+			if ( isset($this->page_access_lookup[$topmenu['url']]) ) {
+				$topmenu['access_level'] = $this->page_access_lookup[$topmenu['url']];
+			}
+			if ( !isset($this->reverse_item_lookup[$topmenu['url']]) ) { //Prefer sub-menus.
+				$this->reverse_item_lookup[$topmenu['url']] = $topmenu;
+			}
+
+			$new_menu[] = $this->convert_to_wp_format($topmenu);
+
+			foreach($topmenu['items'] as $item) {
+				if ( isset($this->page_access_lookup[$item['url']]) ) {
+					$item['access_level'] = $this->page_access_lookup[$item['url']];
+				}
+				$this->reverse_item_lookup[$item['url']] = $item;
+				$new_submenu[$topmenu['file']][] = $this->convert_to_wp_format($item);
 			}
 		}
 
@@ -864,19 +948,8 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 		$item = ameMenuItem::apply_defaults($item);
 		$item = ameMenuItem::apply_filters($item, $item_type, $parent); //may cause side-effects
 
-		//Check if the current user can access this menu.
-		$user_has_access = true;
-		$cap_to_use = '';
-		if ( !empty($item['access_level']) ) {
-			$user_has_access = $user_has_access && $this->current_user_can($item['access_level']);
-			$cap_to_use = $item['access_level'];
-		}
-		if ( !empty($item['extra_capability']) ) {
-			$user_has_access = $user_has_access && $this->current_user_can($item['extra_capability']);
-			$cap_to_use = $item['extra_capability'];
-		}
-
-		$item['access_level'] = $user_has_access ? $cap_to_use : 'do_not_allow';
+		$item['access_level'] = $this->get_menu_capability($item);
+		$this->add_access_lookup($item, $item['access_level'], $item_type);
 
 		//Used later to determine the current page based on URL.
 		$item['url'] = ameMenuItem::generate_url($item['file'], $parent);
@@ -890,6 +963,38 @@ class WPMenuEditor extends MenuEd_ShadowPluginFramework {
 		}
 
 		return $item;
+	}
+
+	/**
+	 * Figure out if the current user can access a menu item and what capability they would need.
+	 *
+	 * This method takes into account both the default capability set by WordPress as well as
+	 * custom role and capability settings specified by the user.
+	 *
+	 * @param array $item Menu item (with defaults applied).
+	 * @return string Required capability, or 'do_not_allow' if the current user can't access this menu.
+	 */
+	private function get_menu_capability($item) {
+		$item['access_level'] = apply_filters(
+			'custom_admin_menu_capability',
+			$item['access_level'],
+			$item
+		);
+
+		//Check if the current user can access this menu.
+		$user_has_access = true;
+		$cap_to_use = '';
+		if ( !empty($item['access_level']) ) {
+			$user_has_access = $user_has_access && $this->current_user_can($item['access_level']);
+			$cap_to_use = $item['access_level'];
+		}
+		if ( !empty($item['extra_capability']) ) {
+			$user_has_access = $user_has_access && $this->current_user_can($item['extra_capability']);
+			$cap_to_use = $item['extra_capability'];
+		}
+
+		$capability = $user_has_access ? $cap_to_use : 'do_not_allow';
+		return $capability;
 	}
 	
   /**
